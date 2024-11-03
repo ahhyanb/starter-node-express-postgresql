8c2d425ba445337a2feebdaaa25f31f51aeb5d19
Ryan Bartolome <rahhyahh@icloud.com>
2024-11-02 20:27:16 -1000

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 const path = require("path");
 require("dotenv").config();
 
-console.log("DEVELOPMENT_DATABASE_URL:", process.env.DEVELOPMENT_DATABASE_URL); // Debugging: check if URL is loaded
+console.log("DEVELOPMENT_DATABASE_URL:", process.env.DEVELOPMENT_DATABASE_URL); // Debugging: Confirm URL
 
 module.exports = {
   development: {
-    client: 'pg',  // Use 'pg' for PostgreSQL
-    connection: process.env.DEVELOPMENT_DATABASE_URL + "?ssl=true", // Add ssl parameter directly to the URL
+    client: 'pg',  // Ensure this is 'pg' for PostgreSQL
+    connection: process.env.DEVELOPMENT_DATABASE_URL, // Pass URL directly
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
